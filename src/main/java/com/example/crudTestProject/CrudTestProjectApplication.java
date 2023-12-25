d431484663cd74bd2b6d54289cace4422bb1e065
antaehyeon <shoe1541@naver.com>
2023-12-25 17:44:49 +0900

    first commit

---
+++ src/main/java/com/example/crudTestProject/CrudTestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudTestProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudTestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudTestProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/crudTestProject/CrudTestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudTestProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudTestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
